2432e899f0cc24f00c4c6effa19786bca15d87a3
F-tao <1185766226@qq.com>
2019-08-01 00:07:10 +0800

    代码格式化

--- src/main/java/co/topc/iems/controller/RoomController.java
+++ src/main/java/co/topc/iems/controller/RoomController.java
@@ -2,18 +2,19 @@ package co.topc.iems.controller;
 
 import co.topc.iems.common.util.PageUtil;
 import co.topc.iems.entity.Room;
-import co.topc.iems.entity.req.RoomListReq;
+import co.topc.iems.entity.req.AddRoomReq;
+import co.topc.iems.entity.req.ListRoomReq;
+import co.topc.iems.entity.req.UpdateRoomReq;
 import co.topc.iems.service.IRoomService;
 import co.topc.web.commons.TopcWebResponse;
+import co.topc.web.commons.constants.TopcWebResponseEnum;
 import co.topc.web.commons.utils.TopcWebResponseUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.Page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author fantao
@@ -25,18 +26,89 @@ public class RoomController {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private String message;
+
     @Autowired
     private IRoomService roomService;
 
+
+
     @GetMapping(value = "/list")
-    public TopcWebResponse getRoomList(RoomListReq roomListReq) {
+    public TopcWebResponse getRoomList(ListRoomReq listRoomReq) {
         if (logger.isInfoEnabled()) {
-            logger.info("getRoomList请求参数{}", JSON.toJSONString(roomListReq));
+            logger.info("getRoomList param{}", JSON.toJSONString(listRoomReq));
         }
         TopcWebResponse topcWebResponse = TopcWebResponseUtil.getSuccess();
-        Page<Room> roomPage = roomService.getRoomList(roomListReq);
+        Page<Room> roomPage = roomService.getRoomList(listRoomReq);
 
         topcWebResponse.setData(PageUtil.getData(roomPage));
+        return topcWebResponse;
+    }
+
+    @GetMapping("/{pkId}")
+    public TopcWebResponse getRoomDetail(@RequestParam("pkId") String pkId) {
+        if (logger.isInfoEnabled()) {
+            logger.info("getRoomDetail param{}", JSON.toJSONString(pkId));
+        }
+        TopcWebResponse topcWebResponse = TopcWebResponseUtil.getSuccess();
+        Room roomDetail = roomService.getRoomDetail(pkId);
+
+        topcWebResponse.setData(roomDetail);
+        return topcWebResponse;
+    }
+
+    @PostMapping
+    public TopcWebResponse addRoom(@RequestBody AddRoomReq addRoomReq) {
+        TopcWebResponse topcWebResponse = TopcWebResponseUtil.getSuccess();
+        if (logger.isInfoEnabled()) {
+            logger.info("addRoom param{}", JSON.toJSONString(addRoomReq));
+        }
+        if (null == addRoomReq) {
+            // 此处应该可以自定义错误原因
+            return TopcWebResponseUtil.getFaile();
+        }
+        try {
+            roomService.addRoom(addRoomReq);
+        } catch (Exception e) {
+            message = "addRoom exception";
+            logger.error(message, e);
+            topcWebResponse.setCode(TopcWebResponseEnum.INSERT_ERROR.getCode());
+            topcWebResponse.setMessage(message);
+        }
+        return topcWebResponse;
+    }
+
+    @PutMapping
+    public TopcWebResponse updateRoom(@RequestBody UpdateRoomReq updateRoomReq) {
+        TopcWebResponse topcWebResponse = TopcWebResponseUtil.getSuccess();
+        if (logger.isInfoEnabled()) {
+            logger.info("updateRoom param{}", JSON.toJSONString(updateRoomReq));
+        }
+        try {
+            roomService.updateRoom(updateRoomReq);
+        } catch (Exception e) {
+            message = "updateRoom exception";
+            logger.error(message, e);
+            topcWebResponse.setCode(TopcWebResponseEnum.UPDATE_ERROR.getCode());
+            topcWebResponse.setMessage(message);
+        }
+        return topcWebResponse;
+    }
+
+    @DeleteMapping("/{pkId}")
+    public TopcWebResponse deleteRoom(@RequestParam("pkId") String pkId) {
+        TopcWebResponse topcWebResponse = TopcWebResponseUtil.getSuccess();
+        if (logger.isInfoEnabled()) {
+            logger.info("deleteRoom param{}", JSON.toJSONString(pkId));
+        }
+        try {
+            roomService.deleteRoom(pkId);
+        } catch (Exception e) {
+            message = "deleteRoom exception";
+            logger.error(message, e);
+            topcWebResponse.setCode(TopcWebResponseEnum.DELETE_ERROR.getCode());
+            topcWebResponse.setMessage(message);
+        }
         return topcWebResponse;
     }
 }
